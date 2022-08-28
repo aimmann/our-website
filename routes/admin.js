e9582823b060be76268dfb721f9bd8dc6b491f37
unknown <wael_saad65@yahoo.com>
2022-08-28 17:02:03 +0200

    change adminbro

--- routes/admin.js
+++ routes/admin.js
@@ -15,7 +15,7 @@ const adminBro = new AdminBro({
   databases: [mongoose],
   rootPath: "/admin",
   branding: {
-    companyName: "BestBags",
+    companyName: "Our Store",
     logo: "/images/shop-icon.png",
     softwareBrothers: false,
   },
